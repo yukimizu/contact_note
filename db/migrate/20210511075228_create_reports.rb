817cfe8260a0aaf11b9024d8e24060b4146ed609
EC2 Default User <ec2-user@ip-172-31-17-150.ap-northeast-1.compute.internal>
2021-05-16 09:41:35 +0000

    reportテーブルの追加

--- db/migrate/20210511075228_create_reports.rb
+++ db/migrate/20210511075228_create_reports.rb
@@ -1,7 +1,9 @@
 class CreateReports < ActiveRecord::Migration[5.2]
   def change
     create_table :reports do |t|
-
+      t.string      :title
+      t.text        :text
+      t.string      :edition
       t.timestamps
     end
   end
